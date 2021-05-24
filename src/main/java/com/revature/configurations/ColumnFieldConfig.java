3d38f9f836c191d05b8c545aa05167ca2334b656
Nicholas Recino <nick.recino@icloud.com>
2021-05-24 16:57:24 -0400

    Removed some extra comments

--- src/main/java/com/revature/configurations/ColumnFieldConfig.java
+++ src/main/java/com/revature/configurations/ColumnFieldConfig.java
@@ -5,26 +5,13 @@
  * Last Modified: 5/14/21, 11:33 PM
  * Created by: Nicholas Recino
  */
-
-/*
- * File: ColumnFieldConfig
- * Team_d_p1_ORM
- * Date created: 5/16/21, 1:32 AM
- * Last Modified: 5/14/21, 11:33 PM
- * Created by: Nicholas Recino
- */
-
-/*
- * File: ColumnFieldConfig
- * Team_d_p1_ORM
- * Date created: 5/14/21, 7:48 PM
- * Last Modified: 5/14/21, 7:45 PM
- * Created by: Nicholas Recino
- */
 package com.revature.configurations;
 
 import com.revature.types.DataType;
 
+/**
+ *
+ */
 public class ColumnFieldConfig {
     private String fieldName;
     private String columnName;
